3816c7abfc52a29a0f01de3c2e3a090637fa7d66
Hooseinsalari <hossein.salari002@gmail.com>
2022-01-07 23:05:11 +0330

    change and finish

--- src/components/TransActionsComponent.js
+++ src/components/TransActionsComponent.js
@@ -8,13 +8,28 @@ import trash from "../svg/trash.svg";
 import { useEffect, useState } from "react/cjs/react.development";
 
 const TransActionsComponent = ({ transActions, setTransActions }) => {
-  const [filteredItem, setFilteredItem] = useState([]);
-  const [search, setSearch] = useState("")
+  const [filteredItem, setFilteredItem] = useState(transActions);
+  const [searchItem, setSearchItem] = useState("")
+
+  const filterTransactions = (search) => {
+    if(!search || search === "") {
+      setFilteredItem(transActions)
+      return  
+    }
+    const filtered = transActions.filter((t) => t.desc.toLowerCase().includes(search.toLowerCase()))
+    setFilteredItem(filtered)
+  }
+
+  const searchHandler = (event) => {
+    setSearchItem(event.target.value)
+    filterTransactions(event.target.value)
+  }
+
 
   useEffect(() => {
-    setFilteredItem(transActions)
+    filterTransactions(searchItem)
   }, [transActions])
-  // -----------------------
+
   const expenseType = filteredItem.filter((t) => t.type === "expense");
   const incomeType = filteredItem.filter((t) => t.type === "income");
 
@@ -36,52 +51,48 @@ const TransActionsComponent = ({ transActions, setTransActions }) => {
     const saveData = JSON.parse(localStorage.getItem("transActions"));
     if (saveData) setTransActions(saveData);
   };
+  
+  if(!transActions.length) {
+    return (<div className={styles.container}>
+      <div className={styles.transactionsCon}>
+        <h3 className={styles.searchError}>Transactions is empty</h3>
+      </div>
+    </div>)
+  }
 
+  
   return (
     <div className={styles.container}>
-      <div className={styles.expContainer}>
-        <h1 className={styles.expHead}>Expense</h1>
-        {transActions.length !== 0 ? (
-          transActions.map((t) => (
+      <div className={styles.transactionsCon}>
+        <div className={styles.form}>
+          <input type="text" placeholder="Search transactions..." value={searchItem} onChange={searchHandler} className={styles.searchInput} />
+        </div>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Transactions</h1>
+        </div>
+        {filteredItem.length ? (
+          filteredItem.map((t) => (
             t.type === "expense" ?
-            <div key={t.id} className={styles.expenseContainer}>
-              <p className={styles.transDesc}>{t.desc}</p>
-              <p className={styles.transAmount}>${t.amount}</p>
-              <button onClick={() => deleteHandler(t.id)}>
-                <img src={trash} alt="logo" />
-              </button>
-            </div> :
-            <div key={t.id} className={styles.incomeContainer}>
-              <p className={styles.transDesc}>{t.desc}</p>
-              <p className={styles.transAmount}>${t.amount}</p>
-              <button onClick={() => deleteHandler(t.id)}>
-                <img src={trash} alt="logo" />
-              </button>
-            </div>
+              <div key={t.id} className={styles.expenseContainer}>
+                <p className={styles.transDesc}>{t.desc}</p>
+                <p className={styles.transAmount}>${t.amount}</p>
+                <button onClick={() => deleteHandler(t.id)}>
+                  <img src={trash} alt="logo" />
+                </button>
+              </div> 
+            :
+              <div key={t.id} className={styles.incomeContainer}>
+                <p className={styles.transDesc}>{t.desc}</p>
+                <p className={styles.transAmount}>${t.amount}</p>
+                <button onClick={() => deleteHandler(t.id)}>
+                  <img src={trash} alt="logo" />
+                </button>
+              </div>
           ))
         ) : (
-          <h3 className={styles.emptyAlert}>Expense is empty</h3>
+          <h3 className={styles.emptyAlert}>Not Matched!</h3>
         )}
       </div>
-
-      {/* <div className={styles.incContainer}>
-        <h1 className={styles.incHead}>Income</h1>
-        {incomeType.length !== 0 ? (
-          incomeType.map((t) => (
-            <div key={t.id} className={styles.incomeContainer}>
-              <p className={styles.transDesc}>{t.desc}</p>
-              <p className={styles.transAmount}>${t.amount}</p>
-              <button onClick={() => deleteHandler(t.id)}>
-                <img src={trash} alt="logo" />
-              </button>
-            </div>
-          ))
-        ) : (
-          <h3 className={styles.emptyAlert}>Income is empty</h3>
-        )}
-      </div> */}
-      {/* <input type="text" value={search} onChange={searchHandler} /> */}
-
     </div>
   );
 };
